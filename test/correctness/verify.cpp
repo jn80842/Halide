a1722536468e4ef6fdf5de5268faa8b309475b24
Julie Newcomb <newcombj@cs.washington.edu>
2018-06-13 18:06:23 -0400

    small harness for experimenting with smt2 printing

---
+++ test/correctness/verify.cpp
@@ -0,0 +1,47 @@
+#include "Halide.h"
+
+using namespace Halide;
+using namespace Halide::Internal;
+
+#define internal_assert _halide_user_assert
+
+void check(const Expr &a, const Expr &b) {
+    std::cout << "called expr check\n";
+    Expr simpler = simplify(a);
+    if (!equal(simpler, b)) {
+        std::cerr
+            << "\nSimplification failure:\n"
+            << "Input: " << a << '\n'
+            << "Output: " << simpler << '\n'
+            << "Expected output: " << b << '\n';
+        abort();
+    }
+}
+
+void check(const Stmt &a, const Stmt &b) {
+    std::cout << "called stmt check\n";
+    Stmt simpler = simplify(a);
+    if (!equal(simpler, b)) {
+        std::cerr
+            << "\nSimplification failure:\n"
+            << "Input: " << a << '\n'
+            << "Output: " << simpler << '\n'
+            << "Expected output: " << b << '\n';
+        abort();
+    }
+}
+
+int main(int argc, char **argv) {
+    Expr x = Var("x"), y = Var("y"), z = Var("z"), w = Var("w");
+    Expr xf = cast<float>(x);
+    Expr yf = cast<float>(y);
+    Expr t = const_true(), f = const_false();
+    Expr b1 = Variable::make(Bool(), "b1");
+    Expr b2 = Variable::make(Bool(), "b2");
+
+    check(x + 0, x);
+
+    printf("Success!\n");
+
+    return 0;
+}
