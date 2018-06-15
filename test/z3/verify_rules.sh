d347df1494b043155cb11286ebdf503f8ed33543
jn80842 <newcombj@cs.washington.edu>
2018-06-15 17:14:53 -0400

    add options to verify rules script

--- test/z3/verify_rules.sh
+++ test/z3/verify_rules.sh
@@ -3,18 +3,36 @@
 regex='\(assert \(not (.*)\)\)'
 unsatregex='unsat'
 
+show_passing='true'
+show_failing='true'
+
+while getopts "fp" flag ; do
+  case $flag in
+    f) show_passing='false' ;;
+    p) show_failing='false' ;;
+  esac
+done
+
 for filename in ../../bin/build/tmp/*.smt2; do
-  echo "---------------------------------------------------------------"
   ASSERTION=`cat ${filename}`
   [[ $ASSERTION =~ $regex ]]
   RELATION=${BASH_REMATCH[1]}
   OUTPUT=`cat harness1.smt2 ${filename} harness2.smt2 | z3 -smt2 -in`
   if [[ $OUTPUT =~ $unsatregex ]] 
   then
-    echo "The relation $RELATION was verified."
+    if [[ "$show_passing" == 'true' ]] ; then
+      echo "---------------------------------------------------------------"
+      echo "Filename: $filename"
+      echo "The relation $RELATION was verified."
+    fi
   else
-    echo "The relation $RELATION could not be verified."
-    echo $OUTPUT
+    if [[ "$show_failing" == 'true' ]] ; then
+      echo "---------------------------------------------------------------"
+      echo "Filename: $filename"
+      echo "The relation $RELATION could not be verified."
+      echo $OUTPUT
+    fi
   fi
 done
 echo "----------------------------------------------------------------"
+
