c44b64b048b7a6c6646a34f8d35a6711afbd5b98
jn80842 <newcombj@cs.washington.edu>
2018-06-27 10:13:53 -0400

    SMT treats broadcasts as their (scalar) value

--- test/correctness/verify.cpp
+++ test/correctness/verify.cpp
@@ -5,6 +5,14 @@ using namespace Halide::Internal;
 
 #define internal_assert _halide_user_assert
 
+Expr ramp(const Expr &base, const Expr &stride, int w) {
+    return Ramp::make(base, stride, w);
+}
+
+Expr broadcast(const Expr &base, int w) {
+    return Broadcast::make(base, w);
+}
+
 void check(const Expr &a, const Expr &b) {
     std::cout << "called expr check\n";
     Expr simpler = simplify(a);
@@ -46,8 +54,13 @@ int main(int argc, char **argv) {
     //check(max(x, y + c0) + c1, max(x + c1, y)); // all rules verified except for those using division
     //check(select(b1, 0, y) == 0,f);
     //check(b1 && b2,b1);
-    check(x - y, 7);
-
+    //heck(x - y, 7);
+    //check(select(broadcast(x), y, z), select(x, y, z));
+    //check(select(Expr(broadcast(b1, 2)), y, z), select(b1, y, z));
+    //check(Broadcast::make(x,3) + Broadcast::make(y,3), Broadcast::make(x + y, 3));
+    //check(ramp(x, y,10) + ramp(z, w,10), ramp(x + z, y + w, 10));
+    check(broadcast(x,3) + broadcast(y,3), broadcast(x + 7, 3));
+    //check(ramp(x, y, 3) + broadcast(z, 3), ramp(x + z, y, 3));
     printf("Success!\n");
 
     return 0;
