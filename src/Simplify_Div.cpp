fe8fe75150bec43563a92111d642f95261436ed3
jn80842 <newcombj@cs.washington.edu>
2018-07-18 14:09:01 -0400

    corrected rule predicates

--- src/Simplify_Div.cpp
+++ src/Simplify_Div.cpp
@@ -68,7 +68,7 @@ Expr Simplify::visit(const Div *op, ExprInfo *bounds) {
               (// Fold repeated division
                rewrite((x / c0) / c2, x / fold(c0 * c2),                          c0 > 0 && c2 > 0 && !overflows(c0 * c2)) ||
                rewrite((x / c0 + c1) / c2, (x + fold(c1 * c0)) / fold(c0 * c2),   c0 > 0 && c2 > 0 && !overflows(c0 * c2) && !overflows(c0 * c1)) ||
-               rewrite((x * c0) / c1, x / fold(c1 / c0),                          c1 % c0 == 0 && c0 > 0) ||
+               rewrite((x * c0) / c1, x / fold(c1 / c0),                          c1 % c0 == 0 && c0 > 0 && c1 / c0 != 0) ||
                // Pull out terms that are a multiple of the denominator
                rewrite((x * c0) / c1, x * fold(c0 / c1),                          c0 % c1 == 0 && c1 > 0) ||
 
@@ -134,11 +134,7 @@ Expr Simplify::visit(const Div *op, ExprInfo *bounds) {
                        can_prove((x % c1 + c0 * (lanes - 1)) / c1 == 0, this)))) ||
              (no_overflow_scalar_int(op->type) &&
               (rewrite(x / -1, -x) ||
-<<<<<<< HEAD
                rewrite(c0 / y, select(y < 0, fold(-c0), c0), c0 == -1 ) ||
-=======
-               rewrite(c0 / y, select(y < 0, fold(-c0), c0), c0 == -1 && can_prove(y != 0, this)) ||
->>>>>>> guard rules against division by zero
                rewrite((x * c0 + c1) / c2,
                        (x + fold(c1 / c0)) / fold(c2 / c0),
                        c2 > 0 && c0 > 0 && c2 % c0 == 0) ||
