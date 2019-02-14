1b8a20bfdf82ee999c88e55661d92123951eb363
Julie Newcomb <newcombj@cs.washington.edu>
2019-02-13 22:30:26 -0800

    reorder term maps to allow more existing rules to have correct ordering

--- src/IRMatch.cpp
+++ src/IRMatch.cpp
@@ -296,8 +296,24 @@ void increment_term(IRNodeType node_type, term_map &m) {
 bool term_map_gt(term_map &m1, term_map &m2) {
     if (m1[IRNodeType::Ramp] != m2[IRNodeType::Ramp]) {
         return m1[IRNodeType::Ramp] > m2[IRNodeType::Ramp];
+    } else if (m1[IRNodeType::Broadcast] != m2[IRNodeType::Broadcast]) {
+        return m1[IRNodeType::Broadcast] > m2[IRNodeType::Broadcast];
     } else if (m1[IRNodeType::Select] != m2[IRNodeType::Select]) {
         return m1[IRNodeType::Select] > m2[IRNodeType::Select];
+    } else if (m1[IRNodeType::Div] != m2[IRNodeType::Div]) {
+        return m1[IRNodeType::Div] > m2[IRNodeType::Div];
+    } else if (m1[IRNodeType::Mul] != m2[IRNodeType::Mul]) {
+        return m1[IRNodeType::Mul] > m2[IRNodeType::Mul];
+    } else if (m1[IRNodeType::Mod] != m2[IRNodeType::Mod]) {
+        return m1[IRNodeType::Mod] > m2[IRNodeType::Mod];
+    } else if (m1[IRNodeType::Sub] != m2[IRNodeType::Sub]) {
+        return m1[IRNodeType::Sub] > m2[IRNodeType::Sub];
+    } else if (m1[IRNodeType::Add] != m2[IRNodeType::Add]) {
+        return m1[IRNodeType::Add] > m2[IRNodeType::Add];
+    } else if (m1[IRNodeType::Max] != m2[IRNodeType::Max]) {
+        return m1[IRNodeType::Max] > m2[IRNodeType::Max];
+    } else if (m1[IRNodeType::Min] != m2[IRNodeType::Min]) {
+        return m1[IRNodeType::Min] > m2[IRNodeType::Min];
     } else if (m1[IRNodeType::Not] != m2[IRNodeType::Not]) {
         return m1[IRNodeType::Not] > m2[IRNodeType::Not];
     } else if (m1[IRNodeType::Or] != m2[IRNodeType::Or]) {
@@ -316,26 +332,10 @@ bool term_map_gt(term_map &m1, term_map &m2) {
         return m1[IRNodeType::NE] > m2[IRNodeType::NE];
     } else if (m1[IRNodeType::EQ] != m2[IRNodeType::EQ]) {
         return m1[IRNodeType::EQ] > m2[IRNodeType::EQ];
-    } else if (m1[IRNodeType::Max] != m2[IRNodeType::Max]) {
-        return m1[IRNodeType::Max] > m2[IRNodeType::Max];
-    } else if (m1[IRNodeType::Min] != m2[IRNodeType::Min]) {
-        return m1[IRNodeType::Min] > m2[IRNodeType::Min];
-    } else if (m1[IRNodeType::Div] != m2[IRNodeType::Div]) {
-        return m1[IRNodeType::Div] > m2[IRNodeType::Div];
-    } else if (m1[IRNodeType::Mul] != m2[IRNodeType::Mul]) {
-        return m1[IRNodeType::Mul] > m2[IRNodeType::Mul];
-    } else if (m1[IRNodeType::Mod] != m2[IRNodeType::Mod]) {
-        return m1[IRNodeType::Mod] > m2[IRNodeType::Mod];
-    } else if (m1[IRNodeType::Sub] != m2[IRNodeType::Sub]) {
-        return m1[IRNodeType::Sub] > m2[IRNodeType::Sub];
-    } else if (m1[IRNodeType::Add] != m2[IRNodeType::Add]) {
-        return m1[IRNodeType::Add] > m2[IRNodeType::Add];
  //   } else if (m1[IRNodeType::Variable] != m2[IRNodeType::Variable]) {
  //       return m1[IRNodeType::Variable] > m2[IRNodeType::Variable];
     } else if (m1[IRNodeType::Cast] != m2[IRNodeType::Cast]) {
         return m1[IRNodeType::Cast] > m2[IRNodeType::Cast];
-    } else if (m1[IRNodeType::Broadcast] != m2[IRNodeType::Broadcast]) {
-        return m1[IRNodeType::Broadcast] > m2[IRNodeType::Broadcast];
     } else if (m1[IRNodeType::FloatImm] != m2[IRNodeType::FloatImm]) {
         return m1[IRNodeType::FloatImm] > m2[IRNodeType::FloatImm];
     } else if (m1[IRNodeType::UIntImm] != m2[IRNodeType::UIntImm]) {
