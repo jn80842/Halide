98dae321415bb957dd73291030683d56b481d825
Julie Newcomb <newcombj@cs.washington.edu>
2020-01-07 13:17:36 -0800

    flags on Add and And rules

--- src/Simplify_Add.cpp
+++ src/Simplify_Add.cpp
@@ -40,87 +40,87 @@ Expr Simplify::visit(const Add *op, ExprInfo *bounds) {
 
         if (EVAL_IN_LAMBDA
             (get_rule_flag("add42", rflag) && (rewrite(x + x, x * 2, "add42")) ||
-             rewrite(ramp(x, y) + ramp(z, w), ramp(x + z, y + w, lanes), "add43") ||
-             rewrite(ramp(x, y) + broadcast(z), ramp(x + z, y, lanes), "add44") ||
-             rewrite(broadcast(x) + broadcast(y), broadcast(x + y, lanes), "add45") ||
-             rewrite(select(x, y, z) + select(x, w, u), select(x, y + w, z + u), "add46") ||
+             (get_rule_flag("add43", rflag) && rewrite(ramp(x, y) + ramp(z, w), ramp(x + z, y + w, lanes), "add43")) ||
+             (get_rule_flag("add44", rflag) && rewrite(ramp(x, y) + broadcast(z), ramp(x + z, y, lanes), "add44")) ||
+             (get_rule_flag("add45", rflag) && rewrite(broadcast(x) + broadcast(y), broadcast(x + y, lanes), "add45")) ||
+             (get_rule_flag("add46", rflag) && rewrite(select(x, y, z) + select(x, w, u), select(x, y + w, z + u), "add46")) ||
              (get_rule_flag("add47", rflag) && rewrite(select(x, c0, c1) + c2, select(x, fold(c0 + c2), fold(c1 + c2)), "add47")) ||
              //             rewrite(select(x, y, c1) + c2, select(x, y + c2, fold(c1 + c2))) ||
-             rewrite(select(x, c0, y) + c2, select(x, fold(c0 + c2), y + c2), "add48") ||
+             (get_rule_flag("add48", rflag) && rewrite(select(x, c0, y) + c2, select(x, fold(c0 + c2), y + c2), "add48")) ||
 
-             rewrite((select(x, y, z) + w) + select(x, u, v), select(x, y + u, z + v) + w, "add51") ||
-             rewrite((w + select(x, y, z)) + select(x, u, v), select(x, y + u, z + v) + w, "add52") ||
-             rewrite(select(x, y, z) + (select(x, u, v) + w), select(x, y + u, z + v) + w, "add53") ||
-             rewrite(select(x, y, z) + (w + select(x, u, v)), select(x, y + u, z + v) + w, "add54") ||
-             rewrite((select(x, y, z) - w) + select(x, u, v), select(x, y + u, z + v) - w, "add55") ||
-             rewrite(select(x, y, z) + (select(x, u, v) - w), select(x, y + u, z + v) - w, "add56") ||
-             rewrite((w - select(x, y, z)) + select(x, u, v), select(x, u - y, v - z) + w, "add57") ||
-             rewrite(select(x, y, z) + (w - select(x, u, v)), select(x, y - u, z - v) + w, "add58") ||
+             (get_rule_flag("add51", rflag) && rewrite((select(x, y, z) + w) + select(x, u, v), select(x, y + u, z + v) + w, "add51")) ||
+             (get_rule_flag("add52", rflag) && rewrite((w + select(x, y, z)) + select(x, u, v), select(x, y + u, z + v) + w, "add52")) ||
+             (get_rule_flag("add53", rflag) && rewrite(select(x, y, z) + (select(x, u, v) + w), select(x, y + u, z + v) + w, "add53")) ||
+             (get_rule_flag("add54", rflag) && rewrite(select(x, y, z) + (w + select(x, u, v)), select(x, y + u, z + v) + w, "add54")) ||
+             (get_rule_flag("add55", rflag) && rewrite((select(x, y, z) - w) + select(x, u, v), select(x, y + u, z + v) - w, "add55")) ||
+             (get_rule_flag("add56", rflag) && rewrite(select(x, y, z) + (select(x, u, v) - w), select(x, y + u, z + v) - w, "add56")) ||
+             (get_rule_flag("add57", rflag) && rewrite((w - select(x, y, z)) + select(x, u, v), select(x, u - y, v - z) + w, "add57")) ||
+             (get_rule_flag("add58", rflag) && rewrite(select(x, y, z) + (w - select(x, u, v)), select(x, y - u, z - v) + w, "add58")) ||
 
-             rewrite((x + c0) + c1, x + fold(c0 + c1), "add60") ||
-             rewrite((x + c0) + y, (x + y) + c0, "add61") ||
-             rewrite(x + (y + c0), (x + y) + c0, "add62") ||
-             rewrite((c0 - x) + c1, fold(c0 + c1) - x, "add63") ||
-             rewrite((c0 - x) + y, (y - x) + c0, "add64") ||
-             rewrite((x - y) + y, x, "add65") ||
-             rewrite(x + (y - x), y, "add66") ||
-             rewrite(x + (c0 - y), (x - y) + c0, "add67") ||
-             rewrite((x - y) + (y - z), x - z, "add68") ||
-             rewrite((x - y) + (z - x), z - y, "add69") ||
-             (!exclude_misordered_rules && rewrite(x + y*c0, x - y*(-c0), c0 < 0 && -c0 > 0, "add70")) ||
-             (!exclude_misordered_rules && rewrite(x*c0 + y, y - x*(-c0), c0 < 0 && -c0 > 0 && !is_const(y), "add71")) ||
-             rewrite(x*y + z*y, (x + z)*y, "add72") ||
-             rewrite(x*y + y*z, (x + z)*y, "add73") ||
-             rewrite(y*x + z*y, y*(x + z), "add74") ||
-             rewrite(y*x + y*z, y*(x + z), "add75") ||
-             (!exclude_misordered_rules && rewrite(x*c0 + y*c1, (x + y*fold(c1/c0)) * c0, c1 % c0 == 0, "add76")) ||
-             (!exclude_misordered_rules && rewrite(x*c0 + y*c1, (x*fold(c0/c1) + y) * c1, c0 % c1 == 0, "add77")) ||
+             (get_rule_flag("add60", rflag) && rewrite((x + c0) + c1, x + fold(c0 + c1), "add60")) ||
+             (get_rule_flag("add61", rflag) && rewrite((x + c0) + y, (x + y) + c0, "add61")) ||
+             (get_rule_flag("add62", rflag) && rewrite(x + (y + c0), (x + y) + c0, "add62")) ||
+             (get_rule_flag("add63", rflag) && rewrite((c0 - x) + c1, fold(c0 + c1) - x, "add63")) ||
+             (get_rule_flag("add64", rflag) && rewrite((c0 - x) + y, (y - x) + c0, "add64")) ||
+             (get_rule_flag("add65", rflag) && rewrite((x - y) + y, x, "add65")) ||
+             (get_rule_flag("add66", rflag) && rewrite(x + (y - x), y, "add66")) ||
+             (get_rule_flag("add67", rflag) && rewrite(x + (c0 - y), (x - y) + c0, "add67")) ||
+             (get_rule_flag("add68", rflag) && rewrite((x - y) + (y - z), x - z, "add68")) ||
+             (get_rule_flag("add69", rflag) && rewrite((x - y) + (z - x), z - y, "add69")) ||
+             (get_rule_flag("add70", rflag) && rewrite(x + y*c0, x - y*(-c0), c0 < 0 && -c0 > 0, "add70")) ||
+             (get_rule_flag("add71", rflag) && rewrite(x*c0 + y, y - x*(-c0), c0 < 0 && -c0 > 0 && !is_const(y), "add71")) ||
+             (get_rule_flag("add72", rflag) && rewrite(x*y + z*y, (x + z)*y, "add72")) ||
+             (get_rule_flag("add73", rflag) && rewrite(x*y + y*z, (x + z)*y, "add73")) ||
+             (get_rule_flag("add74", rflag) && rewrite(y*x + z*y, y*(x + z), "add74")) ||
+             (get_rule_flag("add75", rflag) && rewrite(y*x + y*z, y*(x + z), "add75")) ||
+             (get_rule_flag("add76", rflag) && rewrite(x*c0 + y*c1, (x + y*fold(c1/c0)) * c0, c1 % c0 == 0, "add76")) ||
+             (get_rule_flag("add77", rflag) && rewrite(x*c0 + y*c1, (x*fold(c0/c1) + y) * c1, c0 % c1 == 0, "add77")) ||
              (no_overflow(op->type) &&
-              (rewrite(x + x*y, x * (y + 1), "add79") ||
-               rewrite(x + y*x, (y + 1) * x, "add80") ||
-               rewrite(x*y + x, x * (y + 1), "add81") ||
-               rewrite(y*x + x, (y + 1) * x, !is_const(x), "add82") ||
-               rewrite((x + c0)/c1 + c2, (x + fold(c0 + c1*c2))/c1, "add83") ||
-               rewrite((x + (y + c0)/c1) + c2, x + (y + fold(c0 + c1*c2))/c1, "add84") ||
-               rewrite(((y + c0)/c1 + x) + c2, x + (y + fold(c0 + c1*c2))/c1, "add85") ||
-               rewrite((c0 - x)/c1 + c2, (fold(c0 + c1*c2) - x)/c1, c0 != 0 && c1 != 0, "add86") || // When c0 is zero, this would fight another rule
-               rewrite(x + (x + y)/c0, (fold(c0 + 1)*x + y)/c0, "add87") ||
-               rewrite(x + (y + x)/c0, (fold(c0 + 1)*x + y)/c0, "add88") ||
-               rewrite(x + (y - x)/c0, (fold(c0 - 1)*x + y)/c0, "add89") ||
-               rewrite(x + (x - y)/c0, (fold(c0 + 1)*x - y)/c0, "add90") ||
-               rewrite((x - y)/c0 + x, (fold(c0 + 1)*x - y)/c0, "add91") ||
-               rewrite((y - x)/c0 + x, (y + fold(c0 - 1)*x)/c0, "add92") ||
-               rewrite((x + y)/c0 + x, (fold(c0 + 1)*x + y)/c0, "add93") ||
-               rewrite((y + x)/c0 + x, (y + fold(c0 + 1)*x)/c0, "add94") ||
-               rewrite(min(x, y - z) + z, min(x + z, y), "add95") ||
-               rewrite(min(y - z, x) + z, min(y, x + z), "add96") ||
-               rewrite(min(x, y + c0) + c1, min(x + c1, y), c0 + c1 == 0, "add97") ||
-               rewrite(min(y + c0, x) + c1, min(y, x + c1), c0 + c1 == 0, "add98") ||
-               rewrite(z + min(x, y - z), min(z + x, y), "add99") ||
-               rewrite(z + min(y - z, x), min(y, z + x), "add100") ||
-               rewrite(z + max(x, y - z), max(z + x, y), "add101") ||
-               rewrite(z + max(y - z, x), max(y, z + x), "add102") ||
-               rewrite(max(x, y - z) + z, max(x + z, y), "add103") ||
-               rewrite(max(y - z, x) + z, max(y, x + z), "add104") ||
-               rewrite(max(x, y + c0) + c1, max(x + c1, y), c0 + c1 == 0, "add105") ||
-               rewrite(max(y + c0, x) + c1, max(y, x + c1), c0 + c1 == 0, "add106") ||
-               rewrite(max(x, y) + min(x, y), x + y, "add107") ||
-               rewrite(max(x, y) + min(y, x), x + y, "add108"))) ||
+              ((get_rule_flag("add79", rflag) && rewrite(x + x*y, x * (y + 1), "add79")) ||
+               (get_rule_flag("add80", rflag) && rewrite(x + y*x, (y + 1) * x, "add80")) ||
+               (get_rule_flag("add81", rflag) && rewrite(x*y + x, x * (y + 1), "add81")) ||
+               (get_rule_flag("add82", rflag) && rewrite(y*x + x, (y + 1) * x, !is_const(x), "add82")) ||
+               (get_rule_flag("add83", rflag) && rewrite((x + c0)/c1 + c2, (x + fold(c0 + c1*c2))/c1, "add83")) ||
+               (get_rule_flag("add84", rflag) && rewrite((x + (y + c0)/c1) + c2, x + (y + fold(c0 + c1*c2))/c1, "add84")) ||
+               (get_rule_flag("add85", rflag) && rewrite(((y + c0)/c1 + x) + c2, x + (y + fold(c0 + c1*c2))/c1, "add85")) ||
+               (get_rule_flag("add86", rflag) && rewrite((c0 - x)/c1 + c2, (fold(c0 + c1*c2) - x)/c1, c0 != 0 && c1 != 0, "add86")) || // When c0 is zero, this would fight another rule
+               (get_rule_flag("add87", rflag) && rewrite(x + (x + y)/c0, (fold(c0 + 1)*x + y)/c0, "add87")) ||
+               (get_rule_flag("add88", rflag) && rewrite(x + (y + x)/c0, (fold(c0 + 1)*x + y)/c0, "add88")) ||
+               (get_rule_flag("add89", rflag) && rewrite(x + (y - x)/c0, (fold(c0 - 1)*x + y)/c0, "add89")) ||
+               (get_rule_flag("add90", rflag) && rewrite(x + (x - y)/c0, (fold(c0 + 1)*x - y)/c0, "add90")) ||
+               (get_rule_flag("add91", rflag) && rewrite((x - y)/c0 + x, (fold(c0 + 1)*x - y)/c0, "add91")) ||
+               (get_rule_flag("add92", rflag) && rewrite((y - x)/c0 + x, (y + fold(c0 - 1)*x)/c0, "add92")) ||
+               (get_rule_flag("add93", rflag) && rewrite((x + y)/c0 + x, (fold(c0 + 1)*x + y)/c0, "add93")) ||
+               (get_rule_flag("add94", rflag) && rewrite((y + x)/c0 + x, (y + fold(c0 + 1)*x)/c0, "add94")) ||
+               (get_rule_flag("add95", rflag) && rewrite(min(x, y - z) + z, min(x + z, y), "add95")) ||
+               (get_rule_flag("add96", rflag) && rewrite(min(y - z, x) + z, min(y, x + z), "add96")) ||
+               (get_rule_flag("add97", rflag) && rewrite(min(x, y + c0) + c1, min(x + c1, y), c0 + c1 == 0, "add97")) ||
+               (get_rule_flag("add98", rflag) && rewrite(min(y + c0, x) + c1, min(y, x + c1), c0 + c1 == 0, "add98")) ||
+               (get_rule_flag("add99", rflag) && rewrite(z + min(x, y - z), min(z + x, y), "add99")) ||
+               (get_rule_flag("add100", rflag) && rewrite(z + min(y - z, x), min(y, z + x), "add100")) ||
+               (get_rule_flag("add101", rflag) && rewrite(z + max(x, y - z), max(z + x, y), "add101")) ||
+               (get_rule_flag("add102", rflag) && rewrite(z + max(y - z, x), max(y, z + x), "add102")) ||
+               (get_rule_flag("add103", rflag) && rewrite(max(x, y - z) + z, max(x + z, y), "add103")) ||
+               (get_rule_flag("add104", rflag) && rewrite(max(y - z, x) + z, max(y, x + z), "add104")) ||
+               (get_rule_flag("add105", rflag) && rewrite(max(x, y + c0) + c1, max(x + c1, y), c0 + c1 == 0, "add105")) ||
+               (get_rule_flag("add106", rflag) && rewrite(max(y + c0, x) + c1, max(y, x + c1), c0 + c1 == 0, "add106")) ||
+               (get_rule_flag("add107", rflag) && rewrite(max(x, y) + min(x, y), x + y, "add107")) ||
+               (get_rule_flag("add108", rflag) && rewrite(max(x, y) + min(y, x), x + y, "add108")))) ||
              (no_overflow_int(op->type) &&
-              (rewrite((x/y)*y + x%y, x, "add110") ||
-               rewrite((z + x/y)*y + x%y, z*y + x, "add111") ||
-               rewrite((x/y + z)*y + x%y, x + z*y, "add112") ||
-               rewrite(x%y + ((x/y)*y + z), x + z, "add113") ||
-               rewrite(x%y + ((x/y)*y - z), x - z, "add114") ||
-               rewrite(x%y + (z + (x/y)*y), x + z, "add115") ||
-               rewrite((x/y)*y + (x%y + z), x + z, "add116") ||
-               rewrite((x/y)*y + (x%y - z), x - z, "add117") ||
-               rewrite((x/y)*y + (z + x%y), x + z, "add118") ||
-               rewrite(x/2 + x%2, (x + 1) / 2, "add119") ||
+              ((get_rule_flag("add110", rflag) && rewrite((x/y)*y + x%y, x, "add110")) ||
+               (get_rule_flag("add111", rflag) && rewrite((z + x/y)*y + x%y, z*y + x, "add111")) ||
+               (get_rule_flag("add112", rflag) && rewrite((x/y + z)*y + x%y, x + z*y, "add112")) ||
+               (get_rule_flag("add113", rflag) && rewrite(x%y + ((x/y)*y + z), x + z, "add113")) ||
+               (get_rule_flag("add114", rflag) && rewrite(x%y + ((x/y)*y - z), x - z, "add114")) ||
+               (get_rule_flag("add115", rflag) && rewrite(x%y + (z + (x/y)*y), x + z, "add115")) ||
+               (get_rule_flag("add116", rflag) && rewrite((x/y)*y + (x%y + z), x + z, "add116")) ||
+               (get_rule_flag("add117", rflag) && rewrite((x/y)*y + (x%y - z), x - z, "add117")) ||
+               (get_rule_flag("add118", rflag) && rewrite((x/y)*y + (z + x%y), x + z, "add118")) ||
+               (get_rule_flag("add119", rflag) && rewrite(x/2 + x%2, (x + 1) / 2, "add119")) ||
 
-               rewrite(x + ((c0 - x)/c1)*c1, c0 - ((c0 - x) % c1), c1 > 0, "add121") ||
-               rewrite(x + ((c0 - x)/c1 + y)*c1, y * c1 - ((c0 - x) % c1) + c0, c1 > 0, "add122") ||
-               rewrite(x + (y + (c0 - x)/c1)*c1, y * c1 - ((c0 - x) % c1) + c0, c1 > 0, "add123") ||
+               (get_rule_flag("add121", rflag) && rewrite(x + ((c0 - x)/c1)*c1, c0 - ((c0 - x) % c1), c1 > 0, "add121")) ||
+               (get_rule_flag("add122", rflag) && rewrite(x + ((c0 - x)/c1 + y)*c1, y * c1 - ((c0 - x) % c1) + c0, c1 > 0, "add122")) ||
+               (get_rule_flag("add123", rflag) && rewrite(x + (y + (c0 - x)/c1)*c1, y * c1 - ((c0 - x) % c1) + c0, c1 > 0, "add123")) ||
                false)))) {
             return mutate(std::move(rewrite.result), bounds);
         }
--- src/Simplify_And.cpp
+++ src/Simplify_And.cpp
@@ -42,84 +42,84 @@ Expr Simplify::visit(const And *op, ExprInfo *bounds) {
          rewrite((x || y) && y, b) ||
          rewrite(y && (x || y), a) ||
 
-         rewrite(x != y && x == y, false) ||
-         rewrite(x != y && y == x, false) ||
-         rewrite((z && x != y) && x == y, false) ||
-         rewrite((z && x != y) && y == x, false) ||
-         rewrite((x != y && z) && x == y, false) ||
-         rewrite((x != y && z) && y == x, false) ||
-         rewrite((z && x == y) && x != y, false) ||
-         rewrite((z && x == y) && y != x, false) ||
-         rewrite((x == y && z) && x != y, false) ||
-         rewrite((x == y && z) && y != x, false) ||
-         rewrite(x && !x, false) ||
-         rewrite(!x && x, false) ||
-         rewrite(y <= x && x < y, false) ||
+         (get_rule_flag("and45", rflag) && rewrite(x != y && x == y, false, "and45")) ||
+         (get_rule_flag("and46", rflag) && rewrite(x != y && y == x, false, "and46")) ||
+         (get_rule_flag("and47", rflag) && rewrite((z && x != y) && x == y, false, "and47")) ||
+         (get_rule_flag("and48", rflag) && rewrite((z && x != y) && y == x, false, "and48")) ||
+         (get_rule_flag("and49", rflag) && rewrite((x != y && z) && x == y, false, "and49")) ||
+         (get_rule_flag("and50", rflag) && rewrite((x != y && z) && y == x, false, "and50")) ||
+         (get_rule_flag("and51", rflag) && rewrite((z && x == y) && x != y, false, "and51")) ||
+         (get_rule_flag("and52", rflag) && rewrite((z && x == y) && y != x, false, "and52")) ||
+         (get_rule_flag("and53", rflag) && rewrite((x == y && z) && x != y, false, "and53")) ||
+         (get_rule_flag("and54", rflag) && rewrite((x == y && z) && y != x, false, "and54")) ||
+         (get_rule_flag("and55", rflag) && rewrite(x && !x, false, "and55")) ||
+         (get_rule_flag("and56", rflag) && rewrite(!x && x, false, "and56")) ||
+         (get_rule_flag("and57", rflag) && rewrite(y <= x && x < y, false, "and57")) ||
          rewrite(x != c0 && x == c1, b, c0 != c1) ||
          // Note: In the predicate below, if undefined overflow
          // occurs, the predicate counts as false. If well-defined
          // overflow occurs, the condition couldn't possibly
          // trigger because c0 + 1 will be the smallest possible
          // value.
-         rewrite(c0 < x && x < c1, false, !is_float(x) && c1 <= c0 + 1) ||
-         rewrite(x < c1 && c0 < x, false, !is_float(x) && c1 <= c0 + 1) ||
-         rewrite(x <= c1 && c0 < x, false, c1 <= c0) ||
-         rewrite(c0 <= x && x < c1, false, c1 <= c0) ||
-         rewrite(c0 <= x && x <= c1, false, c1 < c0) ||
-         rewrite(x <= c1 && c0 <= x, false, c1 < c0) ||
+         (get_rule_flag("and64", rflag) && rewrite(c0 < x && x < c1, false, !is_float(x) && c1 <= c0 + 1, "and64")) ||
+         (get_rule_flag("and65", rflag) && rewrite(x < c1 && c0 < x, false, !is_float(x) && c1 <= c0 + 1, "and65")) ||
+         (get_rule_flag("and66", rflag) && rewrite(x <= c1 && c0 < x, false, c1 <= c0, "and66")) ||
+         (get_rule_flag("and67", rflag) && rewrite(c0 <= x && x < c1, false, c1 <= c0, "and67")) ||
+         (get_rule_flag("and68", rflag) && rewrite(c0 <= x && x <= c1, false, c1 < c0, "and68")) ||
+         (get_rule_flag("and69", rflag) && rewrite(x <= c1 && c0 <= x, false, c1 < c0, "and69")) ||
          rewrite(c0 < x && x != c1, a, c1 <= c0) ||
-         rewrite(c0 < x && c1 < x, fold(max(c0, c1)) < x) ||
-         rewrite(c0 <= x && c1 <= x, fold(max(c0, c1)) <= x) ||
-         rewrite(x < c0 && x < c1, x < fold(min(c0, c1))) ||
-         rewrite(x <= c0 && x <= c1, x <= fold(min(c0, c1))))) {
+         (get_rule_flag("and71", rflag) && rewrite(c0 < x && c1 < x, fold(max(c0, c1)) < x, "and71")) ||
+         (get_rule_flag("and72", rflag) && rewrite(c0 <= x && c1 <= x, fold(max(c0, c1)) <= x, "and72")) ||
+         (get_rule_flag("and73", rflag) && rewrite(x < c0 && x < c1, x < fold(min(c0, c1)), "and73")) ||
+         (get_rule_flag("and74", rflag) && rewrite(x <= c0 && x <= c1, x <= fold(min(c0, c1)), "and74")))) {
         return rewrite.result;
     }
 
-    if (rewrite(broadcast(x) && broadcast(y), broadcast(x && y, op->type.lanes())) ||
+    if ((get_rule_flag("and78", rflag) && rewrite(broadcast(x) && broadcast(y), broadcast(x && y, op->type.lanes()), "and78"))||
 
-        rewrite(c0 < x && x < c1, x == fold(c0 + 1), !is_float(x) && c1 == c0 + 2) ||
-        rewrite(c0 <= x && x < c1, x == c0, !is_float(x) && c1 == c0 + 1) ||
-        rewrite(c0 < x && x <= c1, x == fold(c0 + 1), !is_float(x) && c1 == c0 + 1) ||
-        rewrite(x < c1 && c0 < x, x == fold(c0 + 1), !is_float(x) && c1 == c0 + 2) ||
-        rewrite(x < c1 && c0 <= x, x == c0, !is_float(x) && c1 == c0 + 1) ||
-        rewrite(x <= c1 && c0 < x, x == fold(c0 + 1), !is_float(x) && c1 == c0 + 1) ||
+        (get_rule_flag("and80", rflag) && rewrite(c0 < x && x < c1, x == fold(c0 + 1), !is_float(x) && c1 == c0 + 2, "and80")) ||
+        (get_rule_flag("and81", rflag) && rewrite(c0 <= x && x < c1, x == c0, !is_float(x) && c1 == c0 + 1, "and81")) ||
+        (get_rule_flag("and82", rflag) && rewrite(c0 < x && x <= c1, x == fold(c0 + 1), !is_float(x) && c1 == c0 + 1, "and82")) ||
+        (get_rule_flag("and83", rflag) && rewrite(x < c1 && c0 < x, x == fold(c0 + 1), !is_float(x) && c1 == c0 + 2, "and83")) ||
+        (get_rule_flag("and84", rflag) && rewrite(x < c1 && c0 <= x, x == c0, !is_float(x) && c1 == c0 + 1, "and84")) ||
+        (get_rule_flag("and85", rflag) && rewrite(x <= c1 && c0 < x, x == fold(c0 + 1), !is_float(x) && c1 == c0 + 1, "and85")) ||
 
-        rewrite(c0 < x && x != c1, c1 < x, !is_float(x) && c1 == c0 + 1) ||
+        (get_rule_flag("and87", rflag) && rewrite(c0 < x && x != c1, c1 < x, !is_float(x) && c1 == c0 + 1, "and87")) ||
 
-        rewrite((x || (y && z)) && y, (x || z) && y) ||
-        rewrite((x || (z && y)) && y, (x || z) && y) ||
-        rewrite(y && (x || (y && z)), y && (x || z)) ||
-        rewrite(y && (x || (z && y)), y && (x || z)) ||
+        (get_rule_flag("and89", rflag) && rewrite((x || (y && z)) && y, (x || z) && y, "and89")) ||
+        (get_rule_flag("and90", rflag) && rewrite((x || (z && y)) && y, (x || z) && y, "and90")) ||
+        (get_rule_flag("and91", rflag) && rewrite(y && (x || (y && z)), y && (x || z), "and91")) ||
+        (get_rule_flag("and92", rflag) && rewrite(y && (x || (z && y)), y && (x || z), "and92")) ||
 
-        rewrite(((y && z) || x) && y, (z || x) && y) ||
-        rewrite(((z && y) || x) && y, (z || x) && y) ||
-        rewrite(y && ((y && z) || x), y && (z || x)) ||
-        rewrite(y && ((z && y) || x), y && (z || x)) ||
+        (get_rule_flag("and94", rflag) && rewrite(((y && z) || x) && y, (z || x) && y, "and94")) ||
+        (get_rule_flag("and95", rflag) && rewrite(((z && y) || x) && y, (z || x) && y, "and95")) ||
+        (get_rule_flag("and96", rflag) && rewrite(y && ((y && z) || x), y && (z || x), "and96")) ||
+        (get_rule_flag("and97", rflag) && rewrite(y && ((z && y) || x), y && (z || x), "and97")) ||
 
-        rewrite((x && (y || z)) && y, x && y) ||
-        rewrite((x && (z || y)) && y, x && y) ||
-        rewrite(y && (x && (y || z)), y && x) ||
-        rewrite(y && (x && (z || y)), y && x) ||
+        (get_rule_flag("and99", rflag) && rewrite((x && (y || z)) && y, x && y, "and99")) ||
+        (get_rule_flag("and100", rflag) && rewrite((x && (z || y)) && y, x && y, "and100")) ||
+        (get_rule_flag("and101", rflag) && rewrite(y && (x && (y || z)), y && x, "and101")) ||
+        (get_rule_flag("and102", rflag) && rewrite(y && (x && (z || y)), y && x, "and102")) ||
 
-        rewrite(((y || z) && x) && y, x && y) ||
-        rewrite(((z || y) && x) && y, x && y) ||
-        rewrite(y && ((y || z) && x), y && x) ||
-        rewrite(y && ((z || y) && x), y && x) ||
+        (get_rule_flag("and104", rflag) && rewrite(((y || z) && x) && y, x && y, "and104")) ||
+        (get_rule_flag("and105", rflag) && rewrite(((z || y) && x) && y, x && y, "and105")) ||
+        (get_rule_flag("and106", rflag) && rewrite(y && ((y || z) && x), y && x, "and106")) ||
+        (get_rule_flag("and107", rflag) && rewrite(y && ((z || y) && x), y && x, "and107")) ||
 
-        rewrite((x || y) && (x || z), x || (y && z)) ||
-        rewrite((x || y) && (z || x), x || (y && z)) ||
-        rewrite((y || x) && (x || z), x || (y && z)) ||
-        rewrite((y || x) && (z || x), x || (y && z)) ||
+        (get_rule_flag("and109", rflag) && rewrite((x || y) && (x || z), x || (y && z), "and109")) ||
+        (get_rule_flag("and110", rflag) && rewrite((x || y) && (z || x), x || (y && z), "and110")) ||
+        (get_rule_flag("and111", rflag) && rewrite((y || x) && (x || z), x || (y && z), "and111")) ||
+        (get_rule_flag("and112", rflag) && rewrite((y || x) && (z || x), x || (y && z), "and112")) ||
 
-        rewrite(x < y && x < z, x < min(y, z)) ||
-        rewrite(y < x && z < x, max(y, z) < x) ||
+        (get_rule_flag("and114", rflag) && rewrite(x < y && x < z, x < min(y, z), "and114")) ||
+        (get_rule_flag("and115", rflag) && rewrite(y < x && z < x, max(y, z) < x, "and115")) ||
 
-        rewrite(x <= y && y <= x, x == y) ||
-        rewrite((z && x <= y) && y <= x, z && (x == y)) ||
-        rewrite((x <= y && z) && y <= x, z && (x == y)) ||
+        (get_rule_flag("and117", rflag) && rewrite(x <= y && y <= x, x == y, "and117")) ||
+        (get_rule_flag("and118", rflag) && rewrite((z && x <= y) && y <= x, z && (x == y), "and118")) ||
+        (get_rule_flag("and119", rflag) && rewrite((x <= y && z) && y <= x, z && (x == y), "and119")) ||
 
-        rewrite(x <= y && x <= z, x <= min(y, z)) ||
-        rewrite(y <= x && z <= x, max(y, z) <= x) ||
+        (get_rule_flag("and120", rflag) && rewrite(x <= y && x <= z, x <= min(y, z), "and120")) ||
+        (get_rule_flag("and121", rflag) && rewrite(y <= x && z <= x, max(y, z) <= x, "and121")) ||
         false) {
 
         return mutate(std::move(rewrite.result), bounds);
