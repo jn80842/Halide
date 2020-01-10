fe1c96281bfbb698ac5060e730d43b3aaac02628
Julie Newcomb <newcombj@cs.washington.edu>
2020-01-09 23:08:50 -0800

    flags on Or rules

--- src/Simplify_Or.cpp
+++ src/Simplify_Or.cpp
@@ -50,67 +50,67 @@ Expr Simplify::visit(const Or *op, ExprInfo *bounds) {
          rewrite((z || (x || y)) || y, a) ||
          rewrite(y || (z || (x || y)), b) ||
 
-         rewrite(x != y || x == y, true) ||
-         rewrite(x != y || y == x, true) ||
-         rewrite((z || x != y) || x == y, true) ||
-         rewrite((z || x != y) || y == x, true) ||
-         rewrite((x != y || z) || x == y, true) ||
-         rewrite((x != y || z) || y == x, true) ||
-         rewrite((z || x == y) || x != y, true) ||
-         rewrite((z || x == y) || y != x, true) ||
-         rewrite((x == y || z) || x != y, true) ||
-         rewrite((x == y || z) || y != x, true) ||
-         rewrite(x || !x, true) ||
-         rewrite(!x || x, true) ||
-         rewrite(y <= x || x < y, true) ||
+         (get_rule_flag("or53", rflag) && rewrite(x != y || x == y, true, "or53")) ||
+         (get_rule_flag("or54", rflag) && rewrite(x != y || y == x, true, "or54")) ||
+         (get_rule_flag("or55", rflag) && rewrite((z || x != y) || x == y, true, "or55")) ||
+         (get_rule_flag("or56", rflag) && rewrite((z || x != y) || y == x, true, "or56")) ||
+         (get_rule_flag("or57", rflag) && rewrite((x != y || z) || x == y, true, "or57")) ||
+         (get_rule_flag("or58", rflag) && rewrite((x != y || z) || y == x, true, "or58")) ||
+         (get_rule_flag("or59", rflag) && rewrite((z || x == y) || x != y, true, "or59")) ||
+         (get_rule_flag("or60", rflag) && rewrite((z || x == y) || y != x, true, "or60")) ||
+         (get_rule_flag("or61", rflag) && rewrite((x == y || z) || x != y, true, "or61")) ||
+         (get_rule_flag("or62", rflag) && rewrite((x == y || z) || y != x, true, "or62")) ||
+         (get_rule_flag("or63", rflag) && rewrite(x || !x, true, "or63")) ||
+         (get_rule_flag("or64", rflag) && rewrite(!x || x, true, "or64")) ||
+         (get_rule_flag("or65", rflag) && rewrite(y <= x || x < y, true, "or65")) ||
          rewrite(x != c0 || x == c1, a, c0 != c1) ||
-         rewrite(x <= c0 || c1 <= x, true, !is_float(x) && c1 <= c0 + 1) ||
-         rewrite(c1 <= x || x <= c0, true, !is_float(x) && c1 <= c0 + 1) ||
-         rewrite(x <= c0 || c1 < x, true, c1 <= c0) ||
-         rewrite(c1 <= x || x < c0, true, c1 <= c0) ||
-         rewrite(x < c0 || c1 < x, true, c1 < c0) ||
-         rewrite(c1 < x || x < c0, true, c1 < c0) ||
-         rewrite(c0 < x || c1 < x, fold(min(c0, c1)) < x) ||
-         rewrite(c0 <= x || c1 <= x, fold(min(c0, c1)) <= x) ||
-         rewrite(x < c0 || x < c1, x < fold(max(c0, c1))) ||
-         rewrite(x <= c0 || x <= c1, x <= fold(max(c0, c1))))) {
+         (get_rule_flag("or67", rflag) && rewrite(x <= c0 || c1 <= x, true, !is_float(x) && c1 <= c0 + 1, "or67")) ||
+         (get_rule_flag("or68", rflag) && rewrite(c1 <= x || x <= c0, true, !is_float(x) && c1 <= c0 + 1, "or68")) ||
+         (get_rule_flag("or69", rflag) && rewrite(x <= c0 || c1 < x, true, c1 <= c0, "or69")) ||
+         (get_rule_flag("or70", rflag) && rewrite(c1 <= x || x < c0, true, c1 <= c0, "or70")) ||
+         (get_rule_flag("or71", rflag) && rewrite(x < c0 || c1 < x, true, c1 < c0, "or71")) ||
+         (get_rule_flag("or72", rflag) && rewrite(c1 < x || x < c0, true, c1 < c0, "or72")) ||
+         (get_rule_flag("or73", rflag) && rewrite(c0 < x || c1 < x, fold(min(c0, c1)) < x, "or73")) ||
+         (get_rule_flag("or74", rflag) && rewrite(c0 <= x || c1 <= x, fold(min(c0, c1)) <= x, "or74")) ||
+         (get_rule_flag("or75", rflag) && rewrite(x < c0 || x < c1, x < fold(max(c0, c1)), "or75")) ||
+         (get_rule_flag("or76", rflag) && rewrite(x <= c0 || x <= c1, x <= fold(max(c0, c1)), "or76")))) {
         return rewrite.result;
     }
 
     if (EVAL_IN_LAMBDA
         (rewrite(broadcast(x) || broadcast(y), broadcast(x || y, op->type.lanes())) ||
 
-         rewrite(x < y || y < x, x != y) ||
+         (get_rule_flag("or83", rflag) && rewrite(x < y || y < x, x != y, "or83")) ||
 
-         rewrite((x && (y || z)) || y, (x && z) || y) ||
-         rewrite((x && (z || y)) || y, (x && z) || y) ||
-         rewrite(y || (x && (y || z)), y || (x && z)) ||
-         rewrite(y || (x && (z || y)), y || (x && z)) ||
+         (get_rule_flag("or85", rflag) && rewrite((x && (y || z)) || y, (x && z) || y, "or85")) ||
+         (get_rule_flag("or86", rflag) && rewrite((x && (z || y)) || y, (x && z) || y, "or86")) ||
+         (get_rule_flag("or87", rflag) && rewrite(y || (x && (y || z)), y || (x && z), "or87")) ||
+         (get_rule_flag("or88", rflag) && rewrite(y || (x && (z || y)), y || (x && z), "or88")) ||
 
-         rewrite(((y || z) && x) || y, (z && x) || y) ||
-         rewrite(((z || y) && x) || y, (z && x) || y) ||
-         rewrite(y || ((y || z) && x), y || (z && x)) ||
-         rewrite(y || ((z || y) && x), y || (z && x)) ||
+         (get_rule_flag("or90", rflag) && rewrite(((y || z) && x) || y, (z && x) || y, "or90")) ||
+         (get_rule_flag("or91", rflag) && rewrite(((z || y) && x) || y, (z && x) || y, "or91")) ||
+         (get_rule_flag("or92", rflag) && rewrite(y || ((y || z) && x), y || (z && x), "or92")) ||
+         (get_rule_flag("or93", rflag) && rewrite(y || ((z || y) && x), y || (z && x), "or93")) ||
 
-         rewrite((x || (y && z)) || y, x || y) ||
-         rewrite((x || (z && y)) || y, x || y) ||
-         rewrite(y || (x || (y && z)), y || x) ||
-         rewrite(y || (x || (z && y)), y || x) ||
+         (get_rule_flag("or95", rflag) && rewrite((x || (y && z)) || y, x || y, "or95")) ||
+         (get_rule_flag("or96", rflag) && rewrite((x || (z && y)) || y, x || y, "or96")) ||
+         (get_rule_flag("or97", rflag) && rewrite(y || (x || (y && z)), y || x, "or97")) ||
+         (get_rule_flag("or98", rflag) && rewrite(y || (x || (z && y)), y || x, "or98")) ||
 
-         rewrite(((y && z) || x) || y, x || y) ||
-         rewrite(((z && y) || x) || y, x || y) ||
-         rewrite(y || ((y && z) || x), y || x) ||
-         rewrite(y || ((z && y) || x), y || x) ||
+         (get_rule_flag("or100", rflag) && rewrite(((y && z) || x) || y, x || y, "or100")) ||
+         (get_rule_flag("or101", rflag) && rewrite(((z && y) || x) || y, x || y, "or101")) ||
+         (get_rule_flag("or102", rflag) && rewrite(y || ((y && z) || x), y || x, "or102")) ||
+         (get_rule_flag("or103", rflag) && rewrite(y || ((z && y) || x), y || x, "or103")) ||
 
-         rewrite((x && y) || (x && z), x && (y || z)) ||
-         rewrite((x && y) || (z && x), x && (y || z)) ||
-         rewrite((y && x) || (x && z), x && (y || z)) ||
-         rewrite((y && x) || (z && x), x && (y || z)) ||
+         (get_rule_flag("or105", rflag) && rewrite((x && y) || (x && z), x && (y || z), "or105")) ||
+         (get_rule_flag("or106", rflag) && rewrite((x && y) || (z && x), x && (y || z), "or106")) ||
+         (get_rule_flag("or107", rflag) && rewrite((y && x) || (x && z), x && (y || z), "or107")) ||
+         (get_rule_flag("or108", rflag) && rewrite((y && x) || (z && x), x && (y || z), "or108")) ||
 
-         rewrite(x < y || x < z, x < max(y, z)) ||
-         rewrite(y < x || z < x, min(y, z) < x) ||
-         rewrite(x <= y || x <= z, x <= max(y, z)) ||
-         rewrite(y <= x || z <= x, min(y, z) <= x))) {
+         (get_rule_flag("or110", rflag) && rewrite(x < y || x < z, x < max(y, z), "or110")) ||
+         (get_rule_flag("or111", rflag) && rewrite(y < x || z < x, min(y, z) < x, "or111")) ||
+         (get_rule_flag("or112", rflag) && rewrite(x <= y || x <= z, x <= max(y, z), "or112")) ||
+         (get_rule_flag("or113", rflag) && rewrite(y <= x || z <= x, min(y, z) <= x, "or113")))) {
 
         return mutate(std::move(rewrite.result), bounds);
     }
