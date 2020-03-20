3e6f43eb54fb2718ef7c6a9d8ea186eb24b8be0e
Julie Newcomb <newcombj@cs.washington.edu>
2020-03-20 00:04:09 -0700

    set up run_all_experiments for all benchmarks

--- apps/rules_experiments/run_all_experiments.sh
+++ apps/rules_experiments/run_all_experiments.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-for app in harris; do # local_laplacian unsharp bilateral_grid camera_pipe nl_means stencil_chain iir_blur interpolate max_filter; do
+for app in harris local_laplacian unsharp bilateral_grid camera_pipe nl_means stencil_chain iir_blur interpolate max_filter; do
     pushd ../${app}
-    bash ../super_simplify/run_experiment.sh 0 2
+    bash ../super_simplify/run_experiment.sh 0 64
     popd
 done
